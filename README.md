22578b6133ff96f85c79417f89646b54da0b5db7
LKBotond <l.kovacs.botond@gmail.com>
2025-07-06 14:48:51 +0300

    added encryption and decryption for strings

---
+++ README.md
@@ -0,0 +1,4 @@
+# Banking App
+
+## TLDR
+This is my first ever java project, its a backend for a banking application. For database interactions I've utilised SQLite3. For user authentication and password hashing, Argon2.
--- src/main/java/Classes/Encrypt/Encrypt.java
+++ src/main/java/Classes/Encrypt/Encrypt.java
@@ -1,12 +1,25 @@
 package Classes.Encrypt;
 
+import java.util.Base64;
+import java.util.ArrayList;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.GCMParameterSpec;
+import java.security.SecureRandom;
+
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 
 public class Encrypt {
+    // Argon2 params
     private int iterations = 4;
     int memory = 131072;
     int parallelism = 2;
+
+    // AES-GCM params
+    int bytes = 12;
+    int bit_Length = 128;
 
     public String HashWithArgon2(String password) {
         Argon2 engine = Argon2Factory.create();
@@ -26,5 +39,46 @@ public class Encrypt {
         } finally {
             engine.wipeArray(pass_Array);
         }
+    }
+
+    public ArrayList<String> encryptWithAESGCM(String data, String key) {
+        byte[] iv = new byte[bytes];
+        new SecureRandom().nextBytes(iv);
+        ArrayList<String> encrypted_And_IV = new ArrayList<>();
+        try {
+            SecretKeySpec secret_Key = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+            GCMParameterSpec gcm_Spec = new GCMParameterSpec(bit_Length, iv);
+
+            cipher.init(Cipher.ENCRYPT_MODE, secret_Key, gcm_Spec);
+
+            byte[] encrypted = cipher.doFinal(data.getBytes());
+            encrypted_And_IV.add(Base64.getEncoder().encodeToString(iv));
+            encrypted_And_IV.add(Base64.getEncoder().encodeToString(encrypted));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return encrypted_And_IV;
+
+    }
+
+    public String decryptGCM(ArrayList<String> encrypted_And_IV, String key) {
+        String decrypted_String = "FAILIURE";
+        try {
+            byte[] iv = Base64.getDecoder().decode(encrypted_And_IV.get(0));
+            byte[] encrypted = Base64.getDecoder().decode(encrypted_And_IV.get(1));
+
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+            GCMParameterSpec gcmSpec = new GCMParameterSpec(bit_Length, iv);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmSpec);
+
+            byte[] decrypted = cipher.doFinal(encrypted);
+            decrypted_String = new String(decrypted);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return decrypted_String;
     }
 }
